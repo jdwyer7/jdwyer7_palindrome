b796af062acb64dd7dcb62e48e78fa2a3f843e0f
Justin Dwyer <jdwyer7@nd.edu>
2019-02-11 03:21:39 +0000

    Finish working and refactoring palindrome method

--- lib/jdwyer7_palindrome.rb
+++ lib/jdwyer7_palindrome.rb
@@ -1,5 +1,16 @@
 require "jdwyer7_palindrome/version"
 
-module Jdwyer7Palindrome
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+  
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.scan(/[a-z]/i).join.downcase
+    end
 end
